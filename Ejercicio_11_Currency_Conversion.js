05d04110a911893dcd72c5e9f11508150ee437d3
agustin <tino@web1.es>
2020-07-20 22:58:50 +0200

    Añadido al ejercicio 11 cuatro paises para elegir y así convertir el montante de los Euros introducidos a la moneda del pais elegido

--- Ejercicio_11_Currency_Conversion.js
+++ Ejercicio_11_Currency_Conversion.js
@@ -3,5 +3,12 @@
 let converts = (euros, rate) => {
   let editRate = rate * 100
   let result = Math.round(euros * editRate) / 100;
+  if (rate === 0.90) {
+    return result + " Libras";
+  } else if (rate === 4.84) {
+    return result + " Ron";
+  } else if (rate === 8) {
+    return result + " Yuan/es";
+  }
   return result + "$"
 };
