a397c998c510d61dfe1cabe4b13e1c3306ca17ad
agustin <tino@web1.es>
2020-07-27 17:21:24 +0200

    Ejercicio 11, movida la llamada a la funcion amountOutput dentro del else para que cuando se cumpla la condición para el caso location.reload() no siga cargando a llamada a la función

--- Ejercicio_11_Currency_Conversion.js
+++ Ejercicio_11_Currency_Conversion.js
@@ -1,15 +1,24 @@
 
 
-let converts = (euros, rate) => {
-  let editRate = rate * 100;
-  let result = Math.round(euros * editRate) / 100;
-  let resultFixed = result.toFixed(2);
-  if (rate === 0.90) {
-    return resultFixed + " Libras";
-  } else if (rate === 4.84) {
-    return resultFixed + " Ron";
-  } else if (rate === 8) {
-    return resultFixed + " Yuan/es";
+let converts = (euros, country) => {
+  let england = 0.90 * 100;
+  let romania = 4.84 * 100;
+  let china = 8 * 100;
+  let usa = 1.3751 * 100;
+  let englandResult = Math.round(euros * england) / 100
+  let romaniaResult = Math.round(euros * romania) / 100
+  let chinaResult = Math.round(euros * china) / 100
+  let usaResult =  Math.round(euros * usa) / 100
+  let englandResultFixed = englandResult.toFixed(2);
+  let romaniaResultFixed = romaniaResult.toFixed(2);
+  let chinaResultFixed = chinaResult.toFixed(2);
+  let usaResultFixed = usaResult.toFixed(2);
+  if (country === "England") {
+    return englandResultFixed + " Libras";
+  } else if (country === "Romania") {
+    return romaniaResultFixed + " Ron";
+  } else if (country === "China") {
+    return chinaResultFixed + " Yuan/es";
   }
-  return resultFixed + "$"
+  return usaResultFixed + "$"
 };
