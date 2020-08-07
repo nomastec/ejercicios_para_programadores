9ea6ccc5496005219fffa6e7aa3058a1395c4b5e
agustin <tino@web1.es>
2020-08-07 13:24:09 +0200

    Ejercicio_14_Tax_Calculator añadido al archivo js la salida cuando el estado no es Wisconsin, y los tests pertinentes para su comprobación

--- Ejercicio_14_Tax_Calculator.js
+++ Ejercicio_14_Tax_Calculator.js
@@ -1,13 +1,14 @@
 
 
 let taxCalculator = (amount, state) => {
+  let amountNumber = parseInt(amount);
+  let rateTax = 5.5 / 100;
+  let totalRateTax = amountNumber * rateTax;
+  let totalRateTaxFixed = totalRateTax.toFixed(2);
+  let totalAmount = amountNumber + totalRateTax;
+  let totalAmountFixed = totalAmount.toFixed(2);
   if (state === "WI" || state === "wi" || state === "Wisconsin" || state === "wisconsin" || state === "WISCONSIN") {
-    let amountNumber = parseInt(amount);
-    let rateTax = 5.5 / 100;
-    let totalRateTax = amountNumber * rateTax;
-    let totalRateTaxFixed = totalRateTax.toFixed(2);
-    let totalAmount = amountNumber + totalRateTax;
-    let totalAmountFixed = totalAmount.toFixed(2);
     return "$" + amountNumber.toFixed(2) + " $" + totalRateTaxFixed + " $" + totalAmountFixed;
   }
+  return "$" + totalAmountFixed;
 };
