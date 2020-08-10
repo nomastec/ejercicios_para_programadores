145700b2ccc39db53983e23bb1b2d266f0b48deb
agustin <tino@web1.es>
2020-08-10 23:46:04 +0200

    Ejercicio_14_Tax_Calculator añadido al archivo js el metodo trim para elimine espacios en blanco al final y/o al principio si el usuario introduce estos por error en la entrada de estados, además añadidos 2 Tests más para verificar su correcto funcionamiento.

--- Ejercicio_14_Tax_Calculator.js
+++ Ejercicio_14_Tax_Calculator.js
@@ -1,13 +1,14 @@
 
 
 let taxCalculator = (amount, state) => {
+  let stateTrim = state.trim();
   let amountNumber = parseInt(amount);
   let rateTax = 5.5 / 100;
   let totalRateTax = amountNumber * rateTax;
   let totalRateTaxFixed = totalRateTax.toFixed(2);
   let totalAmount = amountNumber + totalRateTax;
   let totalAmountFixed = totalAmount.toFixed(2);
-  if (state.toLowerCase() === "wi" || state.toLowerCase() === "wisconsin") {
+  if (stateTrim.toLowerCase() === "wi" || stateTrim.toLowerCase() === "wisconsin") {
     return "The subtotal is $" + amountNumber.toFixed(2) + "\n The tax is $" + totalRateTaxFixed + "\n The total is $" + totalAmountFixed;
   }
   return "The total is $" + totalAmountFixed;
