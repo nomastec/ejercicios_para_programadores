e38936a7c3fb45a52f5c80cfa34ecff10e5a0d61
agustin <tino@web1.es>
2020-08-17 12:52:36 +0200

    Ejercicio_16_Legal_Driving_Age.js añadido parseInt al parametro age para convertirlo a número y poder usar el operador de comparacion

--- Ejercicio_16_Legal_Driving_Age.js
+++ Ejercicio_16_Legal_Driving_Age.js
@@ -1,6 +1,7 @@
 
 let legalAgeDriving = (age) => {
-  if (age === "15") {
+  let ageInNumber = parseInt(age);
+  if (ageInNumber <= 15) {
     return "You are not old enough to legally drive."
   }
   return "You are old enough to legally drive."
