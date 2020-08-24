f93577e6f53dc2e23cea3abc5441b624c50bceb5
agustin <tino@web1.es>
2020-08-24 14:55:42 +0200

    Ejercicio_14_Tax_Calculator.js añadir variable al metodo lowerCase() para el parametro state.

--- Ejercicio_14_Tax_Calculator.js
+++ Ejercicio_14_Tax_Calculator.js
@@ -2,13 +2,14 @@
 
 let taxCalculator = (amount, state) => {
   let stateTrim = state.trim();
+  let stateResult = stateTrim.toLowerCase();
   let amountNumber = parseInt(amount);
   let rateTax = 5.5 / 100;
   let totalRateTax = amountNumber * rateTax;
   let totalRateTaxFixed = totalRateTax.toFixed(2);
   let totalAmount = amountNumber + totalRateTax;
   let totalAmountFixed = totalAmount.toFixed(2);
-  if (stateTrim.toLowerCase() === "wi" || stateTrim.toLowerCase() === "wisconsin") {
+  if (stateResult === "wi" || stateResult === "wisconsin") {
     return "The subtotal is $" + amountNumber.toFixed(2) + "\n The tax is $" + totalRateTaxFixed + "\n The total is $" + totalAmountFixed;
   }
   return "The total is $" + totalAmountFixed;
